071bab162fdf8ac06fa5e25549caa18b1eb53972
Javier Enrique Perez Sosa <94088560+JavierEnriquePerez@users.noreply.github.com>
2021-11-11 22:14:44 -0700

    Add files via upload

---
+++ practicaJavaScript/practicaJavaScript.js
@@ -0,0 +1,10 @@
+console.log("se cargo el archvo de Javascript");
+
+var contador = 0;
+
+function sumaContador(){
+contador++;
+console.log(contador);
+document.getElementById("contador").innerHTML = contador;
+
+}
